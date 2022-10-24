7c4e2dd003e326340f33d9cf4ff175090d995984
noobtube266 <durranh12@icloud.com>
2022-10-24 11:51:31 -0400

    More notes

---
+++ src/main/java/onlineExamples/BoundedTypes.java
@@ -0,0 +1,16 @@
+package onlineExamples;
+
+public class BoundedTypes <T extends Number> {
+
+    public void display() {
+        System.out.println("This is a bounded type generics class.");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
+        // create an object of GenericsClass
+        GenericsClass<String> obj = new GenericsClass<>();
+    }
+}
